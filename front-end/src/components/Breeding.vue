86e3758e48422833885e35f581b89474df6a6ea8
Amanda <amandamoore829@gmail.com>
2018-01-31 22:14:48 -0600

    Stopping point

--- front-end/src/components/Breeding.vue
+++ front-end/src/components/Breeding.vue
@@ -21,8 +21,7 @@
           <td>{{breeding.date}}</td>
           <td>{{breeding.method}}</td>
           <td>{{breeding.bull}}</td>
-          <td><span data-toggle="modal" data-target="#editBreedingModal" class="icon"><i class="fa fa-2x fa-pencil"></i></span></td>
-
+          <td><router-link :to="{path: '/breeding/' + breeding._id}"><span data-toggle="modal" data-target="#editBreedingModal" class="icon"><i class="fa fa-2x fa-pencil"></i></span></router-link></td>
         </tr>
       </tbody>
     </table>
@@ -52,6 +51,7 @@
               <div class="form-group">
                 <label for="addBreedingMethod">Type</label>
                 <select v-model="newBreeding.method" class="form-control" id="addBreedingMethod">
+                  <option disabled value="">Select breeding method</option>
                   <option>AI</option>
                   <option>Embryo</option>
                   <option>Natural</option>
@@ -79,11 +79,11 @@
       </div>
     </div>
     <!-- EDIT/DELETE Modal -->
-    <div class="modal fade" id="editBreedingModal" tabindex="-1" role="dialog" aria-labelledby="editBreedingModalLabel" aria-hidden="true">
+    <!-- <div class="modal fade" id="editBreedingModal" tabindex="-1" role="dialog" aria-labelledby="editBreedingModalLabel" aria-hidden="true">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="editBreedingModalLabel">Update Animal</h5>
+            <h5 class="modal-title" id="editBreedingModalLabel">Edit Breeding</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
@@ -92,73 +92,46 @@
             <form>
               <div class="form-group">
                 <label for="editBreedingTagId">Ear Tag Number</label>
-                <input  v-model:value="cow.tag_id" type="text" class="form-control" id="editBreedingTagId" placeholder="Ear tag number">
-              </div>
-              <div class="form-group">
-                <label for="editBreedingType">Type</label>
-                <select v-model:value="cow.type"  class="form-control" id="editBreedingType">
-                  <option disabled value="">Select type of animal</option>
-                  <option>Cow</option>
-                  <option>Bull</option>
-                  <option>Calf- bull</option>
-                  <option>Calf- heifer</option>
+                <input  v-model:value="breedings.tag_id" type="text" class="form-control" id="editBreedingTagId" placeholder="Ear tag number">
+              </div> -->
+              <!-- <div class="form-group">
+                <label for="editBreedingMethod">Method</label>
+                <select v-model:value="breeding.method"  class="form-control" id="editBreedingMethod">
+                  <option disabled value="">Select breeding method</option>
+                  <option>AI</option>
+                  <option>Embryo</option>
+                  <option>Natural</option>
                 </select>
-              </div>
-              <div class="form-group">
+              </div> -->
+              <!-- <div class="form-group">
                 <label for="editBreedingDOB">Date of Birth</label>
-                <input v-model:value="cow.dob"  type="date" class="form-control" id="editBreedingDOB" placeholder="mm/dd/yyyy">
+                <input v-model:value="breeding.dob"  type="date" class="form-control" id="editBreedingDOB" placeholder="mm/dd/yyyy">
               </div>
               <div class="form-group">
                 <label for="editBreedingPasture">Pasture</label>
-                <select v-model:value="cow.pasture"  class="form-control" id="editBreedingPasture">
+                <select v-model:value="breeding.pasture"  class="form-control" id="editBreedingPasture">
                   <option disabled value="">Select a pasture</option>
                   <option v-for="pasture in pastures">{{pasture.name}}</option>
                 </select>
               </div>
               <div class="form-group">
                 <label for="editBreedingSire">Sire</label>
-                <input v-model:value="cow.sire"  type="text" class="form-control" id="editBreedingSire" placeholder="Sire">
+                <input v-model:value="breeding.sire"  type="text" class="form-control" id="editBreedingSire" placeholder="Sire">
               </div>
               <div class="form-group">
                 <label for="editBreedingDam">Dam</label>
-                <input v-model:value="cow.dam" type="text" class="form-control" id="editBreedingDam" placeholder="Dam">
-              </div>
-            </form>
-          </div>
-          <div class="modal-body">
-            <div class="form-group">
-              <label for="editBreedingStatus">Status</label>
-              <select v-model:value="cow.status"  class="form-control" id="editBreedingStatus">
-                <option disabled value="">Select animal status</option>
-                <option>Active</option>
-                <option>Sold</option>
-                <option>Deceased</option>
-              </select>
-            </div>
-            <div class="form-group">
-              <label for="editBreedingStatusChangeDate">Status Change Date</label>
-              <input v-model:value="cow.status_date" type="date" class="form-control" id="editBreedingStatusChangeDate">
-            </div>
-            <div class="form-group">
-              <label for="editBreedingWeight">Weight</label>
-              <input v-model:value="cow.weight" type="text" class="form-control" id="editBreedingWeight">
-            </div>
-            <div class="form-group">
-              <label for="editBreedingCauseOfDeath">Cause of Death</label>
-              <input v-model:value="cow.causeOfDeath" type="text" class="form-control" id="editBreedingCauseOfDeath">
-            </div>
-            <div class="form-group">
-              <label for="editBreedingStatusComments">Status change comments</label>
-              <input v-model:value="cow.status_comments" type="text" class="form-control" id="editBreedingStatusComments">
-            </div>
-          </div>
-          <div class="modal-footer">
+                <input v-model:value="breeding.dam" type="text" class="form-control" id="editBreedingDam" placeholder="Dam">
+              </div> -->
+            <!-- </form>
+          </div> -->
+
+          <!-- <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
             <button type="button" class="btn btn-primary" @click="editBreeding()">Add to database</button>
-          </div>
-        </div>
+          </div> -->
+        <!-- </div>
       </div>
-    </div>
+    </div> -->
 </div>
 </template>
 
@@ -205,8 +178,39 @@ export default {
         .catch((err) => {
           console.log(err.response);
         });
+    },
+    deleteBreeding() {
+      // console.log(this.$route.params.id);
+      axios.delete('http://127.0.0.1:3000/breeding/' + this.$route.params.id)
+        .then((response) => {
+          console.log(response);
+          window.location.href = '/breeding';
+        })
+        .catch((err) => {
+          console.log(err.response);
+        });
+
+    },
+    editBreeding() {
+      console.log('testing edit breeding method');
+      axios.patch('http://127.0.0.1:3000/breeding/' + this.$route.params.id, {
+          tag_id: this.breeding.tag_id,
+          date: this.breeding.date,
+          method: this.breeding.method,
+          bull: this.breeding.bull,
+          technician: this.breeding.technician,
+          comments: this.breeding.comments,
+
+        })
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.log(error.response);
+        });
     }
   },
+
   beforeCreate() {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {} else {
