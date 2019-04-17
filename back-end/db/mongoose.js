d145aefca6f17373ae23d8926fc0605489d854d2
Amanda <amandamoore829@gmail.com>
2019-04-16 22:57:02 -0500

    limiting repo to UI only

--- back-end/db/mongoose.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-//CONNECTS TO MONGO DB
-let mongooseConnect = require('mongoose');
-mongooseConnect.Promise = global.Promise;
-mongooseConnect.connect('mongodb://amandamoore3:<password>.mlab.com:15124/cattletracks', { useNewUrlParser: true });
-mongooseConnect.set('useCreateIndex', true);
-
-module.exports = { mongooseConnect };
